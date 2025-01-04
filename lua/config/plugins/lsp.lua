7c7a904f4716ff19fe52b1a2ed6c1f192a7b643e
Rafael M Cunha <me@rafaelmc.net>
2025-01-05 00:22:51 +0100

    install blink.cmp

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -1,38 +1,41 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      {
-        "folke/lazydev.nvim",
-        ft = "lua", -- only load on lua files
-        opts = {
-          library = {
-            -- See the configuration section for more details
-            -- Load luvit types when the `vim.uv` word is found
-            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-          },
-        },
-      },
-    },
-    config = function()
-      require("lspconfig").lua_ls.setup {}
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			'saghen/blink.cmp',
+			{
+				"folke/lazydev.nvim",
+				ft = "lua", -- only load on lua files
+				opts = {
+					library = {
+						-- See the configuration section for more details
+						-- Load luvit types when the `vim.uv` word is found
+						{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+					},
+				},
+			},
+		},
+		config = function()
+			local capabilities = require('blink.cmp').get_lsp_capabilities()
 
-      vim.api.nvim_create_autocmd('LspAttach', {
-        callback = function(args)
-          local client = vim.lsp.get_client_by_id(args.data.client_id)
-          if not client then return end
+			require("lspconfig").lua_ls.setup { capabilities = capabilities }
 
-          if client:supports_method('textDocument/formatting') then
-            -- Format the current buffer on save
-            vim.api.nvim_create_autocmd('BufWritePre', {
-              buffer = args.buf,
-              callback = function()
-                vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
-              end,
-            })
-          end
-        end,
-      })
-    end,
-  }
+			vim.api.nvim_create_autocmd('LspAttach', {
+				callback = function(args)
+					local client = vim.lsp.get_client_by_id(args.data.client_id)
+					if not client then return end
+
+					if client:supports_method('textDocument/formatting') then
+						-- Format the current buffer on save
+						vim.api.nvim_create_autocmd('BufWritePre', {
+							buffer = args.buf,
+							callback = function()
+								vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+							end,
+						})
+					end
+				end,
+			})
+		end,
+	}
 }
