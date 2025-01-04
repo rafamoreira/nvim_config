35ee8dc9e67261738bed0ed33c7e4e8bdc844bf4
Rafael M Cunha <me@rafaelmc.net>
2025-01-04 17:21:54 +0100

    temp fix for inspect and change default clipboard

--- init.lua
+++ init.lua
@@ -1,5 +1,9 @@
 require("config.lazy")
 
+-- temporary fix for inspect
+vim.hl = vim.highlight
+
+vim.opt.clipboard = "unnamedplus"
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<space>x", ":.lua<CR>")
 vim.keymap.set("v", "<space>x", ":lua<CR>")
