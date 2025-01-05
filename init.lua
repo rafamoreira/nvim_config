e45afcbf5d2130c12fec16be6c94fab555b589b6
Rafael M Cunha <me@rafaelmc.net>
2025-01-05 18:33:47 +0100

    add gitsigns

--- init.lua
+++ init.lua
@@ -1,16 +1,30 @@
-require("config.lazy")
-
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-vim.opt.clipboard = "unnamedplus"
+require("config.lazy")
+
+vim.opt.clipboard = "unnamedplus" -- Use system clipboard
+vim.opt.cursorline = true         -- Highlight current line
+vim.opt.inccommand = 'split'      -- Show live preview of substitution
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", extends = ">", precedes = "<", nbsp = "␣" }
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.showmode = false
+vim.opt.signcolumn = "yes"
 vim.opt.undofile = true
 
-vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
-vim.keymap.set("n", "<space>x", ":.lua<CR>")
-vim.keymap.set("v", "<space>x", ":lua<CR>")
+local wk = require("which-key")
+wk.add({
+	{ "<leader>",          group = "Leader" },
+	{ "<leader><leader>",  group = "Source" },
+	{ "<Esc>",             "<cmd>nohlsearch<CR>",     desc = "Clear search highlights",        mode = "n" },
+	{ "<leader>q",         vim.diagnostic.setloclist, desc = "Open diagnostic [Q]uickfix list" },
+	{ "<leader><leader>x", "<cmd>source %<CR>",       desc = "Source current file",            mode = "n" },
+	{ "<leader><leader>l", ":.lua<CR>",               desc = "Execute current line",           mode = "n" },
+	{ "<leader><leader>s", ":lua<CR>",                desc = "Execute selection",              mode = "v" },
+})
+
 
 -- Highlight when yanking text
 -- See `:help vim.highlight.on_yank()`
